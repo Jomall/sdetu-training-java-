30838e186ad8dd6197c23ec6307d9a510aab09ad
Jomall <76658382+Jomall@users.noreply.github.com>
2023-08-22 11:58:12 -0400

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.print("Hello world");
 		System.out.println("Am Adding Code To my project ");
+		System.out.println("Adding code fro Git Hub");
 	}
 
 }
