a79dc9c832751fc70bd4cdf070856530f1ba9c62
Jomall <jomalljack@hotmail.com>
2023-08-22 11:52:46 -0400

    Modified HelloWorld app

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,6 +3,7 @@ package basics;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.print("Hello world");
+		System.out.println("Am Adding Code To my project ");
 	}
 
 }
